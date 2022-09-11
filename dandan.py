3a16eda21cc2899608a1a1fa53ebed9c50198d13
tai <41464391+tai769@users.noreply.github.com>
2022-09-11 16:55:39 +0800

    Update dandan.py

--- dandan.py
+++ dandan.py
@@ -8,7 +8,7 @@ import random
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
-city = os.environ['CITYD']
+city = os.environ['CITY']
 birthday = os.environ['BIRTHDAY']
 
 app_id = os.environ["APP_ID"]
